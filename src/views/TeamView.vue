2fbaaa4e146415e3ff29740ad2bff00f785ad6e3
daz7474 <daz7474@gmail.com>
2024-05-21 17:01:07 +0100

    Custom template for modal

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,18 +3,35 @@ import TeamHeader from "@/components/teams/TeamHeader.vue";
 import TeamMembers from "@/components/teams/TeamMembers.vue";
 import TeamFooter from "@/components/teams/TeamFooter.vue";
 import { useTeamStore } from "@/stores/TeamStore";
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
 
 let team = useTeamStore();
 
 team.fill();
+
+let showModal = ref(false);
 </script>
 
 <template>
-  <TeamHeader />
+  <TeamHeader @add="showModal = true" />
 
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamMembers />
   </div>
 
   <TeamFooter />
+
+  <Modal :show="showModal" @close="showModal = false">
+    <template #default>
+      <p>Need to add a new memeber to your team?</p>
+
+      <form class="mt-6">
+        <div class="flex gap-2">
+          <input type="email" placeholder="Email Address..." class="flex-1">
+          <button>Add</button>
+        </div>
+      </form>
+    </template>
+  </Modal>
 </template>
