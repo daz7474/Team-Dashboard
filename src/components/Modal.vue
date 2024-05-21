240622113f36b8a1a8961cb154c2c4b08b5d9d06
daz7474 <daz7474@gmail.com>
2024-05-21 16:43:02 +0100

    Initial commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  show: Boolean
+});
+</script>
+
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-container">
+      <header>Header Text</header>
+
+      <div>Main Text</div>
+
+      <footer>Footer</footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, .6);
+  display: grid;
+  place-items: center;
+}
+
+.modal-container {
+  background: white;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 500px;
+}
+</style>
