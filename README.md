1290d88b95aa60a449cd0863fa723e5d4359a3b1
daz7474 <daz7474@gmail.com>
2024-05-21 16:23:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Team-Dashboard
+
+Template to get started developing with Vue 3 in Vite.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/TeamView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TeamHeader from "@/components/teams/TeamHeader.vue";
+import TeamMembers from "@/components/teams/TeamMembers.vue";
+import TeamFooter from "@/components/teams/TeamFooter.vue";
+import { useTeamStore } from "@/stores/TeamStore";
+
+let team = useTeamStore();
+
+team.fill();
+</script>
+
+<template>
+  <TeamHeader />
+
+  <div class="place-self-center flex flex-col gap-y-3">
+    <TeamMembers />
+  </div>
+
+  <TeamFooter />
+</template>
